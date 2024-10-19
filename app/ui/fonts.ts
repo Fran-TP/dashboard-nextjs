16c184dd12f7ab556f3382d7a49784c092a0ecf9
Fran-TP <ftorresp@unasam.edu.pe>
2024-10-18 21:30:57 -0500

    Add font imports and exports

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana, Poppins } from 'next/font/google'
+
+export const interFont = Inter({ subsets: ['latin'] })
+export const lusitanaFont = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
+export const poppinsFont = Poppins({ weight: '400', subsets: ['latin'] })
