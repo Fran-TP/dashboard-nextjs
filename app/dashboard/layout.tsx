c2df8c632ba777edafbb1bafa4d13e9354652126
Fran-TP <ftorresp@unasam.edu.pe>
2024-10-27 12:36:43 -0500

    change: layout now is a server component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,35 +1,28 @@
-'use client'
-
 import SideNav from '@/app/ui/dashboard/sidenav'
-import { usePathname } from 'next/navigation'
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const pathname = usePathname()
-  const pathSegments = pathname.split('/')
-
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-slate-100">
+    <div className="flex h-dvh flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
       <div className="flex flex-col flex-grow">
-        <div className="hidden md:block text-xl p-4">
-          {pathSegments.map((segment, index) => {
-            const lastIndex = pathSegments.length - 1
-
-            return (
-              <span className="last:text-blue-500 text-gray-500" key={segment}>
-                {segment}
-                {index < lastIndex && <strong>{' / '}</strong>}
-              </span>
-            )
-          })}
+        <div className="p-6 md:overflow-y-auto md:p-12">
+          {/* <AnimatePresence mode="wait" initial={false}>
+            <motion.div
+              key={pathname}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.3 }}
+            > */}
+          {children}
+          {/* </motion.div>
+          </AnimatePresence> */}
         </div>
-        <div className="p-6 md:overflow-y-auto md:p-12">{children}</div>
       </div>
     </div>
   )
