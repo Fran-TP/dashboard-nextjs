b34b18981a844e9a3770a7d4e16593b8281e4a34
Fran-TP <ftorresp@unasam.edu.pe>
2024-11-01 23:00:47 -0500

    refactor: simplify skeleton component styles by removing unused shimmer class

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,12 +1,6 @@
-// Loading animation
-const shimmer =
-  'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent'
-
 export function CardSkeleton() {
   return (
-    <div
-      className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
-    >
+    <div className="shimmer relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm">
       <div className="flex p-4">
         <div className="h-5 w-5 rounded-md bg-gray-200" />
         <div className="ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium" />
@@ -17,7 +11,6 @@ export function CardSkeleton() {
     </div>
   )
 }
-
 export function CardsSkeleton() {
   return (
     <>
@@ -31,7 +24,7 @@ export function CardsSkeleton() {
 
 export function RevenueChartSkeleton() {
   return (
-    <div className={`${shimmer} relative w-full overflow-hidden md:col-span-4`}>
+    <div className="shimmer relative w-full overflow-hidden md:col-span-4">
       <div className="mb-4 h-8 w-36 rounded-md bg-gray-100" />
       <div className="rounded-xl bg-gray-100 p-4">
         <div className="mt-0 grid h-[410px] grid-cols-12 items-end gap-2 rounded-md bg-white p-4 sm:grid-cols-13 md:gap-4" />
@@ -61,9 +54,7 @@ export function InvoiceSkeleton() {
 
 export function LatestInvoicesSkeleton() {
   return (
-    <div
-      className={`${shimmer} relative flex w-full flex-col overflow-hidden md:col-span-4`}
-    >
+    <div className="shimmer relative flex w-full flex-col overflow-hidden md:col-span-4">
       <div className="mb-4 h-8 w-36 rounded-md bg-gray-100" />
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-100 p-4">
         <div className="bg-white px-6">
@@ -85,9 +76,7 @@ export function LatestInvoicesSkeleton() {
 export default function DashboardSkeleton() {
   return (
     <>
-      <div
-        className={`${shimmer} relative mb-4 h-8 w-36 overflow-hidden rounded-md bg-gray-100`}
-      />
+      <div className="relative mb-4 h-8 w-36 overflow-hidden rounded-md bg-gray-100" />
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <CardSkeleton />
         <CardSkeleton />
