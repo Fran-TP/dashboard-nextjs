daca0ed5a0cef1d4bed0ddaceb2703abc350310f
Fran-TP <ftorresp@unasam.edu.pe>
2024-11-01 23:01:51 -0500

    refactor: clean up LatestInvoices component by removing commented code and simplifying class names

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,6 +1,5 @@
 import { fetchLatestInvoices } from '@/app/lib/data'
 import { ArrowPathIcon } from '@heroicons/react/24/outline'
-import clsx from 'clsx'
 import Image from 'next/image'
 export default async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices()
@@ -9,19 +8,12 @@ export default async function LatestInvoices() {
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className="mb-4 text-xl md:text-2xl">Latest Invoices</h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: Uncomment this code in Chapter 7 */}
-
-        <div className="bg-white px-6">
-          {latestInvoices.map((invoice, i) => {
+        <div className="bg-white px-6 divide-y-[1px]">
+          {latestInvoices.map(invoice => {
             return (
               <div
                 key={invoice.id}
-                className={clsx(
-                  'flex flex-row items-center justify-between py-4',
-                  {
-                    'border-t': i !== 0
-                  }
-                )}
+                className="flex flex-row items-center justify-between py-4"
               >
                 <div className="flex items-center">
                   <Image
