dec4332ed92095758b57b6444608a13c225ee85e
Fran-TP <ftorresp@unasam.edu.pe>
2024-11-01 22:59:31 -0500

    feat: disable experimental PPR in dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,6 +4,8 @@ import SideNav from '@/app/ui/dashboard/sidenav'
 import { AnimatePresence, motion } from 'framer-motion'
 import { usePathname } from 'next/navigation'
 
+export const experimental_ppr = true
+
 interface LayoutProps {
   children: React.ReactNode
 }
