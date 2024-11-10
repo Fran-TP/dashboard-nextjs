1cb7736cf4a45015879739c95ff5d509ccec7278
Fran-TP <ftorresp@unasam.edu.pe>
2024-11-10 01:52:04 -0500

    refactor: update visibility breakpoints for InvoicesTableSkeleton component

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -157,7 +157,7 @@ export function InvoicesTableSkeleton() {
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-          <div className="md:hidden">
+          <div className="xl:hidden">
             <InvoicesMobileSkeleton />
             <InvoicesMobileSkeleton />
             <InvoicesMobileSkeleton />
@@ -165,7 +165,7 @@ export function InvoicesTableSkeleton() {
             <InvoicesMobileSkeleton />
             <InvoicesMobileSkeleton />
           </div>
-          <table className="hidden min-w-full text-gray-900 md:table">
+          <table className="hidden min-w-full text-gray-900 xl:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
