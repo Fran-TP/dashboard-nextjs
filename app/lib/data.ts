4e9893203478eb40fbf2e6c9257e2758333c58e5
Fran-TP <ftorresp@unasam.edu.pe>
2024-11-10 02:11:51 -0500

    fix: correct invoice fetching logic to return a single invoice object

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -155,13 +155,13 @@ export async function fetchInvoiceById(id: string) {
       WHERE invoices.id = ${id};
     `
 
-    const invoice = data.rows.map(invoice => ({
+    const [invoice] = data.rows.map(invoice => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100
     }))
 
-    return invoice[0]
+    return invoice
   } catch (error) {
     console.error('Database Error:', error)
     throw new Error('Failed to fetch invoice.')
