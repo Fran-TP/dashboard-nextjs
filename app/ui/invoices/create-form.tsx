0361dcb2d042a1be92434a73e537cf2fbc8aa191
Fran-TP <ftorresp@unasam.edu.pe>
2024-11-10 00:13:54 -0500

    refactor: enhance create invoice form with improved state management and error handling

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,19 +1,55 @@
-import { CustomerField } from '@/app/lib/definitions';
-import Link from 'next/link';
+'use client'
+
+import type { CustomerField } from '@/app/lib/definitions'
+import Link from 'next/link'
+import { useActionState, useEffect, useState } from 'react'
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
+  UserCircleIcon
+} from '@heroicons/react/24/outline'
+import { Button } from '@/app/ui/button'
+import { createInvoice, type State } from '@/app/lib/actions'
+import ErrorMessage from '../forms/error-message'
+import clsx from 'clsx'
+import { Spin } from '../icons/spin'
+
+const initialState: State = {
+  message: null,
+  errors: {},
+  values: {}
+}
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const [state, formAction, isPending] = useActionState(
+    createInvoice,
+    initialState
+  )
+  const [errors, setErrors] = useState<State['errors']>(state?.errors)
+  const [values, setValues] = useState<State['values']>(state?.values)
+
+  useEffect(() => {
+    setErrors(state?.errors)
+    setValues(state?.values)
+  }, [state])
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedUserId = event.target.value
+    setValues({ ...values, customerId: selectedUserId })
+
+    if (errors?.customerId) {
+      const updatedErrors = { ...errors, customerId: undefined }
+
+      setErrors(updatedErrors)
+    }
+  }
+
   return (
-    <form>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
-        <div className="mb-4">
+        <div className="mb-2">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
           </label>
@@ -23,22 +59,26 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              value={values?.customerId}
+              onChange={handleSelectChange}
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
               </option>
-              {customers.map((customer) => (
+              {customers.map(customer => (
                 <option key={customer.id} value={customer.id}>
                   {customer.name}
                 </option>
               ))}
             </select>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            <UserCircleIcon className="peer-focus:text-gray-900 pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          <ErrorMessage id="customer-error" message={errors?.customerId} />
         </div>
 
         {/* Invoice Amount */}
-        <div className="mb-4">
+        <div className="mb-2">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
             Choose an amount
           </label>
@@ -49,12 +89,20 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 name="amount"
                 type="number"
                 step="0.01"
+                defaultValue={values?.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                onChange={() => {
+                  if (errors?.amount) {
+                    setErrors({ ...errors, amount: undefined })
+                  }
+                }}
+                aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          <ErrorMessage id="amount-error" message={errors?.amount} />
         </div>
 
         {/* Invoice Status */}
@@ -71,6 +119,13 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  defaultChecked={values?.status === 'pending'}
+                  onChange={() => {
+                    if (errors?.status) {
+                      setErrors({ ...errors, status: undefined })
+                    }
+                  }}
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="pending"
@@ -86,6 +141,13 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  onChange={() => {
+                    if (errors?.status) {
+                      setErrors({ ...errors, status: undefined })
+                    }
+                  }}
+                  defaultChecked={values?.status === 'paid'}
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="paid"
@@ -96,6 +158,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
           </div>
+          <ErrorMessage id="status-error" message={errors?.status} />
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
@@ -105,8 +168,21 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         >
           Cancel
         </Link>
-        <Button type="submit">Create Invoice</Button>
+        <Button
+          type="submit"
+          className={clsx('w-36 gap-1', { 'cursor-not-allowed': isPending })}
+          disabled={isPending}
+        >
+          {isPending ? (
+            <>
+              <Spin />
+              Processing...
+            </>
+          ) : (
+            'Create Invoice'
+          )}
+        </Button>
       </div>
     </form>
-  );
+  )
 }
