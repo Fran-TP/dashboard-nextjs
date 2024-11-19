8320cf9cf317bf56554a82eaaaf6b4a306588c3b
Fran-TP <ftorresp@unasam.edu.pe>
2024-11-19 18:00:22 -0500

    feat: add metadata for Invoices page with title

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,11 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons'
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
 import { Suspense } from 'react'
 import { fetchInvoicesPages } from '@/app/lib/data'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Invoices'
+}
 
 interface SearchProps {
   query?: string
