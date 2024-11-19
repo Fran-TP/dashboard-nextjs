6a12f942e03e5544a4437212da8b8b3eedff0f8f
Fran-TP <ftorresp@unasam.edu.pe>
2024-11-19 18:00:11 -0500

    feat: add metadata for Customers page with title and description

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Customers',
+  description: 'Customers page'
+}
+
 const Page = () => {
   return <p>Customers Page</p>
 }
